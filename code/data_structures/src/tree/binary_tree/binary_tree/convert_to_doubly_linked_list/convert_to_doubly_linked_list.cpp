a537045665a97975994b4f415786b5eb86ca6db5
Sonali Gupta <49031160+SonaliGupta25@users.noreply.github.com>
2020-05-10 00:51:43 +0530

    Update convert_to_doubly_linked_list.cpp
    Added comments in order to make the code robust

--- code/data_structures/src/tree/binary_tree/binary_tree/convert_to_doubly_linked_list/convert_to_doubly_linked_list.cpp
+++ code/data_structures/src/tree/binary_tree/binary_tree/convert_to_doubly_linked_list/convert_to_doubly_linked_list.cpp
@@ -14,15 +14,16 @@
 template<typename Node>
 void BToDLL(Node *root, Node **head_ref)
 {
+    //firstly checking the base condition 
     if (!root)
         return;
-
+// recursive call to the function now proving right child as one of the parameter
     BToDLL(root->right, head_ref);
     root->right = *head_ref;
-
+// checking if there is a head reference node 
     if (*head_ref)
         (*head_ref)->left = root;
     *head_ref = root;
-
+//recursive call to the function now with the left child of root node
     BToDLL(root->left, head_ref);
 }
